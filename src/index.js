9c3fd005ae621a3d9816abd51b578d800f83a251
Aleksander Krawiel <aleksander.krawiel@makimo.pl>
2020-06-26 15:51:24 +0200

    Send API url as flag to Elm

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
+import dotenv from 'dotenv'
+
 import { Elm } from './elm/Main.elm'
 
+dotenv.config()
+
 Elm.Main.init({
-  node: document.querySelector('main')
+  node: document.querySelector('main'),
+  flags: {
+    apiUrl: process.env.API_URL
+  }
 })
