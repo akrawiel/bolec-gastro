5116999f26c0fa770aed60e750f3b20689bc58a7
Aleksander Krawiel <aleksander.krawiel@makimo.pl>
2020-07-02 12:52:40 +0200

    Slightly change page not found site

--- src/elm/Pages/NotFound.elm
+++ src/elm/Pages/NotFound.elm
@@ -1,10 +1,13 @@
 module Pages.NotFound exposing (view)
 
-import Html exposing (Html, div, text)
-import Html.Attributes exposing (class)
+import Html exposing (Html, a, div, text)
+import Html.Attributes exposing (class, href)
 
 
 view : Html msg
 view =
-    div [ class "width-full height-full flex justify-center align-center font-size-xl color-light" ]
-        [ text "Page not found" ]
+    div [ class "flex justify-center align-center column height-full color-light" ]
+        [ div [ class "font-size-xl font-family-cursive mb-xl" ] [ text "Bolec Gastro" ]
+        , div [ class "font-size-lg mb-md" ] [ text "Page not found" ]
+        , a [ class "button font-size-lg", href "/" ] [ text "Go to main page" ]
+        ]
